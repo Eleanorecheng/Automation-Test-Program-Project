c6a0c8d516073795f8d5b5501b2bdc6c387af9b1
echeng <echeng@netbase.com>
2023-05-21 18:55:50 +0800

    week5_part_2 Update pytest.ini

--- exec_test.sh
+++ exec_test.sh
@@ -2,8 +2,7 @@
 
 python3 -m venv env
 source env/bin/activate
-pip3 install allure-pytest
 pip3 install -r requirements.txt
 rm -rf ./report
-pytest ./tests_web/test_sample.py --alluredir ./report
-allure serve report
+pytest ./tests_web/test_sample.py --alluredir ./allure-results
+# allure serve report
