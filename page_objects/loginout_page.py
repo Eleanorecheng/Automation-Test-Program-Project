0da9c4676ac7bf674f83e60ddc17f8c770e371d1
echeng <echeng@netbase.com>
2023-06-04 17:31:09 +0800

    week_7_part_2 1. Add login abd logout test cases

---
+++ page_objects/loginout_page.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+from selenium.webdriver.common.by import By
+from utils.page_base import PageBase
+from utils.database_utils import DatabaseUtil
+
+
+class LoginoutPage(PageBase):
+    database_util = DatabaseUtil()
+    email_field = (By.XPATH, "//input[@id='email']")
+    password_field = (By.XPATH, "//input[@id='pw']")
+    login_btn = (By.XPATH, "//button[@class='login100-form-btn']")
+    logout_btn = (By.XPATH, "//*[@class='profile__content']/button")
+
+    def input_email_and_password_to_login(self, email, password):
+        elem_email = self.find_element(self.email_field)
+        self.input_and_send_key(elem_email, email)
+        elem_password = self.find_element(self.password_field)
+        self.input_and_send_key(elem_password, password)
+        elem_login_btn = self.find_element(self.login_btn, clickable=True)
+        elem_login_btn.click()
+
+    def get_local_storage(self, key):
+        jwt_token = self.driver.execute_script("return window.localStorage.getItem(arguments[0]);", "jwtToken")
+        return jwt_token
+
+    def set_local_storage(self, key, value):
+        self.driver.execute_script("window.localStorage.setItem(arguments[0], arguments[1]);", key, value)
+
+    def click_logout_btn(self):
+        elem_logout_btn = self.find_element(self.logout_btn, clickable=True)
+        elem_logout_btn.click()
+
+    def get_access_token_from_db(self, db_cursor):
+        sql = f"SELECT access_token from user where email = '{os.getenv('EMAIL')}'"
+        # 預期只會拿回一筆 access_token, 而 get_db_result 是回傳一個 list, 因此取第一筆
+        return self.database_util.get_db_result(db_cursor, sql, "access_token")[0]
--- tests_web/conftest.py
+++ tests_web/conftest.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,6 +8,7 @@ from allure_commons.types import AttachmentType
 import mysql.connector
 import os
 from dotenv import load_dotenv
+from page_objects.loginout_page import LoginoutPage
 
 load_dotenv()
 
@@ -37,5 +39,16 @@ def db_cursor():
     cursor.close()
     db.close()
 
+@pytest.fixture()
+def login(driver, request):
+    driver.get(f"{os.getenv('DOMAIN')}/login.html")
 
+    loginout_page = LoginoutPage(driver)
+    # request 為固定寫法，用於接收 parametrize 中傳入的數值, 即為 {"email": os.environ.get('EMAIL'), "password": os.environ.get('PASSWORD')}
+    email = request.param.get("email")
+    password = request.param.get("password")
+
+    with allure.step("Input email and password to login"):
+        loginout_page.input_email_and_password_to_login(email, password)
+        time.sleep(1) # 不放的話抓不到 alert message --> "error":"no such alert"
 
