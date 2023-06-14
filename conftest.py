30c2166afc265ac81f99ab3ffe713298f1e9c5a4
echeng <echeng@netbase.com>
2023-06-15 00:42:47 +0800

    week_9_part_1 Update os.environ setting

--- conftest.py
+++ conftest.py
@@ -1,8 +1,12 @@
 import mysql.connector
 import os
 import pytest
+from dotenv import load_dotenv
 
-
+if 'ENV_FILE' in os.environ:
+    load_dotenv(os.environ['ENV_FILE'])
+else:
+    load_dotenv()
 @pytest.fixture()
 def db_cursor():
     db = mysql.connector.connect(
