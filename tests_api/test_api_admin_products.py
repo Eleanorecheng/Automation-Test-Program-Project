1b79f09a491195a859da80a42bd0d3506ec3874a
echeng <echeng@netbase.com>
2023-06-24 19:04:35 +0800

    week_10_part_2 1. Update PR comment in week10part1 2. Add cases for create and delete products

---
+++ tests_api/test_api_admin_products.py
@@ -0,0 +1,93 @@
+import pytest
+
+from api_objects.create_product_api import CreateProductsAPI
+from api_objects.delete_product_api import DeleteProductAPI
+from utils.common_handler import APICommonHandler
+from utils.database_utils import DatabaseUtil
+
+from test_data.test_data_from_excel import TestData
+import allure
+
+test_data = TestData()
+
+
+@allure.story("Scenario: Test Create Product Success")
+@pytest.mark.parametrize('product_data', test_data.read_data('API Create Product Success'))
+def test_create_product_success(session, db_cursor, default_api_login, product_data, request):
+    create_product_api = CreateProductsAPI(session)
+    delete_product_api = DeleteProductAPI(session)
+    api_command_handlr = APICommonHandler()
+    database_utils = DatabaseUtil()
+
+    with allure.step("Send create product API request and check status = 200"):
+        info = create_product_api.create_product(product_data)
+        api_command_handlr.assertion(info.response.status_code, 200)
+
+    with allure.step("Get Product_id"):
+        product_id = info.get_json('data')['product_id']
+        assert product_id
+
+    with allure.step("Verify response is correct by comparing with DB"):
+        db_data = database_utils.get_product_info_by_id_from_db(db_cursor, product_id)
+        # 因為 db 撈不到缺 other_images 的情況，所以把 '' 拿掉以利比對
+        payload = create_product_api.arrange_payload(product_data)
+        if '' in payload['other_images']:
+            payload['other_images'].remove('')
+        assert db_data == payload
+
+    def delete_product_finalizer():
+        with allure.step("Delete product and check delete alert"):
+            delete_product_api.delete_product(product_id)
+
+    request.addfinalizer(delete_product_finalizer)
+
+
+@allure.story("Scenario: Test Create Product Fail")
+@pytest.mark.parametrize('product_data', test_data.read_data('API Create Product Failed'))
+def test_create_product_fail(session, db_cursor, default_api_login, product_data, request):
+    create_product_api = CreateProductsAPI(session)
+    delete_product_api = DeleteProductAPI(session)
+    api_command_handlr = APICommonHandler()
+
+    with allure.step("Send create product API request and check status code"):
+        info = create_product_api.create_product(product_data)
+        api_command_handlr.assertion(info.response.status_code, 400)
+        api_command_handlr.assertion(info.get_json('errorMsg'), product_data['Error Msg'])
+
+    with allure.step("Delete product if product_id exists"):
+        try:
+            product_id = info.get_json('data')['product_id']
+            def delete_product_finalizer():
+                with allure.step("Delete product and check delete alert"):
+                    delete_product_api.delete_product(product_id)
+
+            request.addfinalizer(delete_product_finalizer)
+        except:
+            pass
+
+@allure.story("Scenario: Test Create Product Without Login")
+@pytest.mark.parametrize('product_data', test_data.read_data('API Create Product Success'))
+@pytest.mark.parametrize("authorization, status_code, err_msg", [("", 401, "Unauthorized"), ("123", 403, "Forbidden"), ("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm92aWRlciI6Im5hdGl2ZSIsIm5hbWUiOiJFbGVhbm9yZUNoZW5nIiwiZW1haWwiOiJ2ZXhpbGxlMDgzMUBnbWFpbC5jb20iLCJwaWN0dXJlIjpudWxsLCJpYXQiOjE2ODc1OTkxNjZ9.5zra2Ij1kIqbPKylz_zSk2NO1ZbZLhg4bMaCL7nPXL4", 403, "Invalid Access Token")])
+def test_create_product_with_invalid_access(session, product_data, request, authorization, status_code, err_msg):
+    create_product_api = CreateProductsAPI(session)
+    delete_product_api = DeleteProductAPI(session)
+    api_command_handlr = APICommonHandler()
+
+    with allure.step("Set token to invalid"):
+        session.headers["Authorization"] = authorization
+
+    with allure.step("Send create product API request and check status code"):
+        info = create_product_api.create_product(product_data)
+        api_command_handlr.assertion(info.response.status_code, status_code)
+        api_command_handlr.assertion(info.get_json('errorMsg'), err_msg)
+
+    with allure.step("Delete product if product_id exists"):
+        try:
+            product_id = info.get_json('data')['product_id']
+            def delete_product_finalizer():
+                with allure.step("Delete product and check delete alert"):
+                    delete_product_api.delete_product(product_id)
+
+            request.addfinalizer(delete_product_finalizer)
+        except:
+            pass
